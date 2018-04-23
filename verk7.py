0ba98596c60cdcd1a52a62c2cc117a5a32da3fed
Eyjolfur00 <eyjolfurar268@nemi.tskoli.is>
2018-04-23 11:00:18 +0000

    Update verk7.py

--- verk7.py
+++ verk7.py
@@ -20,27 +20,4 @@ def data():
         return template("admin_site.tpl")
 
 
-
-
-
-    
-@route("/setcookie")
-def index():
-    response.set_cookie("Kaka","nammikaka")
-    return "búa til köku"
-
-@route("/deletecookie")
-def index():
-    response.set_cookie("Kaka","", expires=0)
-    return "Kakan er farin"
-
-
-
-
-
-
-@route('/Myndir/<filename>')
-def server_static(filename):
-    return static_file(filename, root="./Myndir")
-
 run(host="0.0.0.0", port=os.environ.get("PORT"))
